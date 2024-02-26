789b1c5bf9b1977f54ba3298d6dd78224ea495dc
Brijesh Wawdhane <brijesh@Brijeshs-MacBook-Pro.local>
2024-02-26 14:59:54 +0400

    test function export

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wbrijesh/go-utils
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello")
+}
+
+func TestFunctionExport() {
+	fmt.Println("test function")
+}
