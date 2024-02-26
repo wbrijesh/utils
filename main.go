0d4dd10cc9e91a00958a073591f08d34cdeb0b9f
Brijesh Wawdhane <brijesh@Brijeshs-MacBook-Pro.local>
2024-02-26 22:10:29 +0400

    feat: new function FunctionTimer

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -10,4 +11,19 @@ func main() {
 
 func TestFunctionExport() {
 	fmt.Println("test function")
+}
+
+func durationToString(duration time.Duration) string {
+	if duration.Seconds() < 1 {
+		return fmt.Sprintf("%d", duration.Milliseconds()) + "ms"
+	}
+	return fmt.Sprintf("%f", duration.Seconds()) + "s"
+}
+
+func FunctionTimer(function func(), functionName string) {
+	startTime := time.Now()
+	function()
+	endTime := time.Now()
+	duration := endTime.Sub(startTime)
+	fmt.Println("\n" + functionName + " took " + durationToString(duration) + " to run")
 }
